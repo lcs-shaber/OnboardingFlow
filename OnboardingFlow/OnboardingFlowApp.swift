eef087af31d77e13e50114ddf8f81e9e3168174b
Sophie Haber <SHaber@lcs.on.ca>
2024-01-17 19:10:06 -0500

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Sophie Haber on 17.01.24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
